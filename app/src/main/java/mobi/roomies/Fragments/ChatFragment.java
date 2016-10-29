b63814f07fb8cd8a23e2219d169e4649a74b4ff6
Kevin Chung <kvnchung92@gmail.com>
2016-10-28 20:03:42 -0500

    temp comit, may break shit I'll fix soon

---
+++ app/src/main/java/mobi/roomies/Fragments/ChatFragment.java
@@ -0,0 +1,68 @@
+package mobi.roomies.Fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import mobi.roomies.R;
+
+/*
+    Created by Kevin Chung
+    Fragment used to contain the chat functionality UI
+    this fragment will utilize firebase to handle messaging via firebase
+ */
+public class ChatFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public ChatFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chat, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    // new instance constructor required to create a new fragment with arguments
+    // change eventually
+    public static ChatFragment newInstance(String page, String title) {
+        ChatFragment chatFragment = new ChatFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, page);
+        args.putString(ARG_PARAM2, title);
+        chatFragment.setArguments(args);
+        return chatFragment;
+    }
+}
