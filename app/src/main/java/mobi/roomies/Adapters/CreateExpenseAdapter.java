1a621879b7321158e09d79fb758cf12e87f1911f
Kevin <kevin.chung@mavs.uta.edu>
2017-04-28 19:02:53 -0500

    pausing for robert

---
+++ app/src/main/java/mobi/roomies/Adapters/CreateExpenseAdapter.java
@@ -0,0 +1,46 @@
+package mobi.roomies.Adapters;
+
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by kevin on 4/28/2017.
+ */
+
+public class CreateExpenseAdapter extends BaseAdapter {
+
+    private String userName, userImageUrl;
+    private Context context;
+    private ArrayList<String> roomies;
+
+    public CreateExpenseAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
